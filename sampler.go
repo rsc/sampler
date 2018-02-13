60bef405c52117ad21d2adb10872b95cf17f8fca
Russ Cox <rsc@golang.org>
2018-02-13 13:05:54 -0500

    sampler: add Hello

---
+++ sampler.go
@@ -0,0 +1,11 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package sampler shows simple texts.
+package sampler // import "rsc.io/sampler"
+
+// Hello returns a greeting.
+func Hello() string {
+	return "Hello, world."
+}
