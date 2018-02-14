f545d0289d06e2add4556ea6a15fc4938014bf87
Russ Cox <rsc@golang.org>
2018-02-14 11:34:12 -0500

    sampler: update docs

--- sampler.go
+++ sampler.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package sampler shows simple texts.
+// Package sampler shows simple texts in a variety of languages.
 package sampler // import "rsc.io/sampler"
 
 import (
